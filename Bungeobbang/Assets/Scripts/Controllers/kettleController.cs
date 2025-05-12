f6eb3b31034c963620d7273b4890d61d22c49cbe
유저이름 <139879153+Azebalue@users.noreply.github.com>
2025-05-13 03:13:25 +0900

    아트 리소스 추가

--- Bungeobbang/Assets/Scripts/Controllers/kettleController.cs
+++ Bungeobbang/Assets/Scripts/Controllers/kettleController.cs
@@ -1,15 +1,13 @@
 using UnityEngine;
 
-public class FillingController : MonoBehaviour
+public class kettleController : MonoBehaviour
 {
-    public GameObject filling;
-    public static FillingController selectedFilling;
     bool isSelected = false;
 
-    Vector3 moveDir = new Vector3(0, 1, 0);
+    Vector3 moveDir = new Vector3(0, 2, 0);
 
-    Vector3 originSpinDir = new Vector3(0, 0, -30);
-    Vector3 spinDir = new Vector3(0, 0, -45);
+    //Vector3 originSpinDir = new Vector3(0, 0, -30);
+    Vector3 spinDir = new Vector3(0, 0, 45);
 
     int originSortingOrder;
     int maxSortingOrder = 10;
@@ -21,32 +19,30 @@ public class FillingController : MonoBehaviour
     }
     private void OnMouseDown()
     {
-        if (selectedFilling == null && isSelected == false)
+        if (isSelected == false)
         {
-            selectFilling();
+            selectKettle();
 
         }
         else if(isSelected == true)
         {
-            deSelectFilling();
+            deSelectKettle();
         }
     }
 
-    void selectFilling()
+    void selectKettle()
     {
         transform.position += moveDir; //위로 올리기
         transform.eulerAngles = spinDir; //약간 회전
         GetComponent<SpriteRenderer>().sortingOrder = maxSortingOrder;
-        selectedFilling = this; 
         isSelected = true;
     }
 
-    void deSelectFilling()
+    void deSelectKettle()
     {
         transform.position -= moveDir; 
-        transform.eulerAngles = originSpinDir;
+        transform.eulerAngles = new Vector3(0,0,0);
         GetComponent<SpriteRenderer>().sortingOrder = originSortingOrder;
-        selectedFilling = null;
         isSelected = false;
     }
 }
